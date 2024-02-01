adf0b755d7a058556fb637e528757118b67aad1d
Sandeep-8312 <88316321+Sandeep-8312@users.noreply.github.com>
2024-02-01 09:49:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 resource "aws_s3_bucket" "example" {
-  bucket = "my-jenkins-bucket"
+  bucket = "madhu-devops123-vcube-114-8am"
 
   tags = {
     Name        = "jenkins-114"
+    Environment = "Dev"
   }
 }
